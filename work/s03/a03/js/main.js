b9f6a9638899edabb4f9141e8c4937410c1c7b52
Yousef Namazi <yousefnamazi@hotmail.com>
2020-01-28 02:24:47 +0100

    Attempt to add municipality (2)

--- work/s03/a03/js/main.js
+++ work/s03/a03/js/main.js
@@ -1,13 +1,29 @@
 (function () {
     'use strict';
 
-    fetch('data/1290.json')
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            console.log(myJson);
-        });
+    document.getElementById('getSchool').addEventListener("click", getData);
 
-    console.log('Sandbox MEGA is ready!');
+    function getData() {
+        fetch('data/1290.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                console.log(myJson);
+                let output = '<h2>Schools</h2>';
+                myJson.forEach(function (school) {
+                    output += `
+                <ul>
+                    <li>Skolenhetskod: ${school.Skolenhetskod}</li>
+                    <li>Skolenhetsnamn: ${school.Skolenhetsnamn}</li>
+                    <li>Kommunkod: ${school.Kommunkod}</li>
+                    <li>PeOrgNr: ${school.PeOrgNr}</li>
+                </ul>
+                `;
+                });
+                document.getElementById('output').innerHTML = output;
+            });
+        console.log('Sandbox MEGA is ready!');
+    }
+
 })();
