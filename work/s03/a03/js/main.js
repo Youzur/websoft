e35542e36d433b27893ed321aafc4b4088dcea73
Yousef Namazi <yousefnamazi@hotmail.com>
2020-01-28 02:40:07 +0100

    Attemp(3)

--- work/s03/a03/js/main.js
+++ work/s03/a03/js/main.js
@@ -9,17 +9,17 @@
                 return response.json();
             })
             .then((myJson) => {
-                console.log(myJson);
+                //console.log(myJson);
                 let output = '<h2>Schools</h2>';
                 myJson.forEach(function (school) {
                     output += `
-                <ul>
-                    <li>Skolenhetskod: ${school.Skolenhetskod}</li>
-                    <li>Skolenhetsnamn: ${school.Skolenhetsnamn}</li>
-                    <li>Kommunkod: ${school.Kommunkod}</li>
-                    <li>PeOrgNr: ${school.PeOrgNr}</li>
-                </ul>
-                `;
+                        <ul>
+                            <li>Skolenhetskod: ${school.Skolenhetskod}</li>
+                            <li>Skolenhetsnamn: ${school.Skolenhetsnamn}</li>
+                            <li>Kommunkod: ${school.Kommunkod}</li>
+                            <li>PeOrgNr: ${school.PeOrgNr}</li>
+                        </ul>
+                    `;
                 });
                 document.getElementById('output').innerHTML = output;
             });
