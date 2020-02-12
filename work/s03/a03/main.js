8c5da1a61bf149c102b725c0eef856b65ae2e71b
Youzur <yousefnamazi@hotmail.com>
2020-02-12 12:53:37 +0100

    Hotfix data

--- work/s03/a03/main.js
+++ work/s03/a03/main.js
@@ -3,27 +3,6 @@
 
     document.getElementById('getSchool').addEventListener("click", getData);
 
-    /*function getData() {
-        fetch('data/1290.json')
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                let output = '<h2>Schools</h2>';
-                data.Skolenheter.forEach(function (school) {
-                    output += `
-                    <ul>
-                        <li>Skolenhetskod: ${school.Skolenhetskod}</li>
-                        <li>Skolenhetsnamn: ${school.Skolenhetsnamn}</li>
-                        <li>Kommunkod: ${school.Kommunkod}</li>
-                        <li>PeOrgNr: ${school.PeOrgNr}</li>
-                    </ul>
-                    `;
-                });
-                document.getElementById('output').innerHTML = output;
-            })
-    }*/
-
-    // UPDATE 1
     function getData() {
         var myList = document.querySelector('ul');
 
@@ -35,6 +14,7 @@
                 return response.json();
             })
             .then(function(json) {
+                console.log(json);
                 for(var i = 0; i < json.Skolenheter.length; i++) {
                     var listItem = document.createElement('li');
                     listItem.innerHTML = '<strong>' + json.Skolenheter[i].Skolenhetskod + '</strong>';
