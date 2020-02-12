d220a32fdd400347a65ab6a857fd39c0b413944e
Youzur <yousefnamazi@hotmail.com>
2020-02-12 10:25:53 +0100

    Modified fetch json

--- work/s03/a03/js/main.js
+++ work/s03/a03/js/main.js
@@ -1,21 +1,13 @@
 (function () {
     'use strict';
 
-
-    fetch('data/1081.json')
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            console.log(myJson);
-        });
-
     document.getElementById('getSchool').addEventListener("click", getData);
 
     function getData() {
         fetch('data/1290.json')
             .then((res) => res.json())
             .then((data) => {
+                console.log(data);
                 let output = '<h2>Schools</h2>';
                 data.Skolenheter.forEach(function (school) {
                     output += `
